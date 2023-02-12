17c9a5eb09d7d834bf4591d8a29e4516aed1751a
rushmanzz <1832753833@qq.com>
2023-02-12 13:55:05 +0800

    init project

---
+++ json/Parser.h
@@ -0,0 +1,46 @@
+/*
+ * @Description: 
+ * @Author: zhangchuangyin
+ * @Date: 2023-02-12 14:17:57
+ * @LastEditTime: 2023-02-12 14:17:58
+ * @LastEditors: zhangchuangyin
+ */
+#pragma once
+
+#include <string>
+using namespace std;
+
+#include "Json.h"
+
+namespace yazi {
+namespace json {
+
+class Parser
+{
+public:
+    Parser();
+    ~Parser();
+
+    void load(const string & str);
+    Json parse();
+
+private:
+    void skip_white_spaces();
+    char get_next_token();
+
+    Json parse_null();
+    Json parse_bool();
+    Json parse_number();
+    Json parse_string();
+
+    bool in_range(long x, long lower, long upper)
+    {
+        return (x >= lower && x <= upper);
+    }
+
+private:
+    string m_str;
+    size_t m_idx;
+};
+
+}}
